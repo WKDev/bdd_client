5be50090ca46e40e03bfdfc8d61c63eaa166466e
wkdev <chanhyukman@gmail.com>
2022-05-21 18:03:21 +0900

    modified

--- gpio.py
+++ gpio.py
@@ -13,17 +13,18 @@ def init_gpio():
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(devs, GPIO.OUT, initial=GPIO.LOW)
 
-def exec_ext():
-    if rd.random() > 0.5:
-        run_sequential()
-    else:
-        run_at_once()
 
 def clear_gpio():
     for dev in devs:
        GPIO.output(dev, GPIO.LOW)
 
+def run_sequential():
+    rd_devs = rd.shuffle(devs)
+    for dev in rd_devs:
+        GPIO.output(dev, GPIO.HIGH)
+        time.sleep(GPIO_INT)
 
+    clear_gpio()
 
 def run_at_once():
     rd_devs = rd.choices(devs, k=rd.choice(range(1,3)))
@@ -33,14 +34,14 @@ def run_at_once():
 
     clear_gpio()
 
+def exec_ext():
+    if rd.random() > 0.5:
+        run_sequential()
+    else:
+        run_at_once()
 
-def run_sequential():
-    rd_devs = rd.shuffle(devs)
-    for dev in rd_devs:
-        GPIO.output(dev, GPIO.HIGH)
-        time.sleep(GPIO_INT)
 
-    clear_gpio()
+
 
 if __name__ == "__main__":
     init_gpio()
